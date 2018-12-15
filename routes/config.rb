863948995f81d0cd37a35b980d4607c599381b21
richez.antoine.richez <richez.antoine.richez@gmail.com>
2018-12-15 18:32:17 -0500

    initial commit

---
+++ routes/config.rb
@@ -0,0 +1,1 @@
+token = 1dYB8xGMAuDDc43RoMzOoOUsdv4UGb7V
---
+++ script.rb
@@ -0,0 +1,10 @@
+require 'discordrb'
+require 'routes/config.rb'
+
+bot = Discordrb::Bot.new token: 'token', client_id: 523637595973091331
+
+bot.message(with_text: 'Ping!') do |event|
+    event.respond 'Pong!'
+end
+
+bot.run
